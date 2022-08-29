33521a72308d1f5a885593c4ec3c237a7fd27697
slavashestakov2005 <slavashestakov2005@gmail.com>
2022-08-29 23:28:18 +0700

    Обновлён парсер

--- excel.py
+++ excel.py
@@ -5,12 +5,16 @@ class ExcelParentWriter:
     def __add_row__(self, worksheet, idx, *args, color=True):
         i = 0
         for arg in args:
-            if arg == 1 and color and i:
-                worksheet.write(idx, i, None, self.green_style)
-            elif arg == -1 and color and i:
-                worksheet.write(idx, i, None, self.red_style)
+            if idx == 1 and i:
+                worksheet.write_url(idx, i, 'https://acmp.ru/?main=user&id={}'.format(arg), self.link_style, str(arg))
+            elif i == 0 and type(arg) == int:
+                worksheet.write_url(idx, i, 'https://acmp.ru/index.asp?main=task&id_task={}'.format(arg), self.link_style, str(arg))
+            elif type(arg) == list and arg[0] == 1 and color and i:
+                worksheet.write(idx, i, arg[1], self.green_style)
+            elif type(arg) == list and arg[0] == -1 and color and i:
+                worksheet.write(idx, i, arg[1], self.red_style)
             elif arg != 0 or not color:
-                worksheet.write(idx, i, arg)
+                worksheet.write(idx, i, arg if type(arg) != list else None)
             i += 1
 
     def __write__(self, worksheet, data, row_idx=0, cols_cnt=0):
@@ -19,7 +23,7 @@ class ExcelParentWriter:
         if not cols_cnt and len(data):
             cols_cnt = len(data[0]) - 1
         for row in data:
-            self.__add_row__(worksheet, row_idx, *row, color=(row_idx <= 1000))
+            self.__add_row__(worksheet, row_idx, *row, color=(row_idx >= 9))
             row_idx += 1
         worksheet.autofilter(start, 0, row_idx-1, cols_cnt)
 
@@ -57,28 +61,34 @@ class ExcelParentWriter:
         self.head_style.set_font_size(14)
         self.green_style = self.workbook.add_format({'bg_color': 'green'})
         self.red_style = self.workbook.add_format({'bg_color': 'red'})
+        self.link_style = self.workbook.add_format({'color': 'blue'})
+        self.link_style.set_underline()
 
 
 class ExcelWriter(ExcelParentWriter):
     def __gen_sheet__(self, worksheet, data: list):
-        head, wr = ['№'], [[i] for i in range(1, 1001)]
-        wr.append(['+'])
-        wr.append(['-'])
-        wr.append(['Место'])
-        wr.append(['Рейтинг'])
+        tasks_count = max(_[1].max_task for _ in data) + 1
+        head, wr = ['№'], [['ID'], ['Место'], ['Рейтинг'], ['Посылки'], ['+ (1000)'], ['- (1000)'], ['+'], ['-']]
+        wr.extend([i] for i in range(1, tasks_count))
         for x in data:
-            head.append(x[0])
-            for i in range(1, 1001):
-                if i in x[1]:
-                    wr[i - 1].append(1)
-                elif i in x[2]:
-                    wr[i - 1].append(-1)
-                else:
-                    wr[i - 1].append(0)
-            wr[1000].append(len(x[1]))
-            wr[1001].append(len(x[2]))
-            wr[1002].append(x[3])
-            wr[1003].append(x[4])
+            user, tasks = x
+            head.append(user.name)
+            wr[0].append(user.id)
+            wr[1].append(user.rank)
+            wr[2].append(user.rating)
+            wr[3].append(user.send)
+            wr[4].append(tasks.goods)
+            wr[5].append(tasks.bads)
+            wr[6].append(tasks.goods + tasks.good)
+            wr[7].append(tasks.bads + tasks.bad)
+            for i in range(1, tasks_count):
+                task = tasks.get_task(i)
+                v = 0
+                if task.good > 0:
+                    v = 1
+                elif task.bad > 0:
+                    v = -1
+                wr[i + 7].append([v, task.pm()])
         self.__head__(worksheet, *head)
         self.__write__(worksheet, wr)
 
@@ -86,5 +96,3 @@ class ExcelWriter(ExcelParentWriter):
         self.__styles__(filename)
         self.__gen_sheet__(self.workbook.add_worksheet('Результаты'), data)
         self.workbook.close()
-
-
--- parser.py
+++ parser.py
@@ -4,40 +4,147 @@ from bs4 import BeautifulSoup
 from excel import ExcelWriter
 
 
-def parse_one(userId):
+class User:
+	def __init__(self, id, name, rank, rating):
+		self.id, self.name, self.rank, self.rating = id, name, rank, rating
+
+	@staticmethod
+	def sortf(user):
+		return user.rank
+
+	def writable(self):
+		return 'ID: {}\nБаллы: {}\nМесто: {}\nПосылок: {}\n'.format(self.id, self.rating, self.rank, self.send)
+
+	def writable_md(self):
+		return '| {} | {} | {} | {} | {} |'.format(self.id, self.name, self.rank, self.rating, self.send)
+
+	def writable_txt(self):
+		return '{}\t{}{}\t{}\t{}'.format(self.id, self.name + ' ' * max(0, 25 - len(self.name)), self.rank, self.rating, self.send)
+
+
+class Task:
+	def __init__(self, id, good, bad):
+		self.id, self.good, self.bad = id, good, bad
+
+	def status(self):
+		return self.good > 0
+
+	@staticmethod
+	def sortf(task):
+		return task.id
+
+	def pm(self):
+		result = ''
+		if self.good > 0:
+			result += '{}+'.format(self.good)
+			if self.bad > 0:
+				result += ', '
+		if self.bad > 0:
+			result += '{}-'.format(self.bad)
+		return result
+
+	def __repr__(self):
+		return '{} ({})'.format(self.id, self.pm())
+
+
+class Tasks:
+	def __init__(self, bad, good):
+		self.map_tasks = {_: Task(_, 0, 0) for _ in set(bad).union(good)}
+		self.max_task = 0
+		for task in good:
+			self.map_tasks[task].good += 1
+		for task in bad:
+			self.map_tasks[task].bad += 1
+		self.good, self.bad, self.goods, self.bads = 0, 0, 0, 0
+		self.tasks = self.map_tasks.values()
+		for task in self.tasks:
+			self.max_task = max(self.max_task, task.id)
+			if task.id <= 1000:
+				if task.status():
+					self.goods += 1
+				else:
+					self.bads += 1
+			else:
+				if task.status():
+					self.good += 1
+				else:
+					self.bad += 1
+
+	def get_task(self, task):
+		if task not in self.map_tasks:
+			return Task(task, 0, 0)
+		return self.map_tasks[task]
+
+	def writable(self):
+		result = 'Всего решено {}, из них в первой тысяче {}\n'.format(self.good + self.goods, self.goods)
+		result += 'Всего не решено {}, из них в первой тысяче {}\n'.format(self.bad + self.bads, self.bads)
+		tasks = {}
+		for task in self.tasks:
+			part = (task.id - 1) // 100
+			if part not in tasks:
+				tasks[part] = [[], []]
+			if task.status():
+				tasks[part][0].append(task)
+			else:
+				tasks[part][1].append(task)
+		tasks = sorted(tasks.items(), key=lambda x: x[0])
+		for part in tasks:
+			p, t = part
+			result += '\nЗадачи {} — {}:\n'.format(p * 100 + 1, p * 100 + 100)
+			good = sorted(t[0], key=Task.sortf)
+			bad = sorted(t[1], key=Task.sortf)
+			result += 'Решено ({}): {}\n'.format(len(good), str(good)[1:-1])
+			result += 'Не решено ({}): {}\n'.format(len(bad), str(bad)[1:-1])
+		return result
+
+	def writable_md(self):
+		return ' {} | {} | {} | {} |\n'.format(self.goods, self.bads, self.goods + self.good, self.bads + self.bad)
+
+	def writable_txt(self):
+		return '\t{}\t   {}\t\t{}\t{}\n'.format(self.goods, self.bads, self.goods + self.good, self.bads + self.bad)
+
+
+def parse_user_profile(userId):
 	url = 'https://acmp.ru/?main=user&id='
 	r = requests.get(url + str(userId))
 	html_parser = BeautifulSoup(r.text, 'html.parser')
-	tasks = html_parser.find_all('p', {'class' : 'text'})[0:2]
-	good = [int(x.text) for x in tasks[0] if x.name]
-	bad = [int(x.text) for x in tasks[1] if x.name]
 	rank =  html_parser.find_all('b', {'class' : 'btext'})[0]
 	rank = int(rank.text.split()[1])
 	rating = html_parser.find_all('b', {'class' : 'btext'})[1]
 	rating = int(rating.text.split()[1])
 	userName = html_parser.find_all('td', {'class' : 'menu_title'})[4].text
-	userName = bytes(userName, r.encoding).decode('cp1251')
-	userName = [s[0].upper() + s[1:].lower() for s in userName.split()]
-	userName = ' '.join(userName[0:2])
-	return [userId, userName, good, bad, rank, rating]
+	userName = bytes(userName, r.encoding).decode('cp1251').title()
+	userName = ' '.join(userName.split(' ')[0:2])
+	return User(userId, userName, rank, rating)
 
 
-def writable(parsed):
-	result = 'Баллы : ' + str(parsed[5]) + '\n'
-	result += 'Место : ' + str(parsed[4]) + '\n'
-	result += 'Решенные задачи (' + str(len(parsed[2])) + '): ' + str(parsed[2])[1:-1] + '\n'
-	result += 'Нерешенные  задачи (' + str(len(parsed[3])) + '): ' + str(parsed[3])[1:-1] + '\n'
-	return result
+def parse_user_submissions(userId):
+	bad_task, good_task = [], []
+	page = 0
+	while True:
+		url = 'https://acmp.ru/index.asp?main=status&id_mem={0}&id_res=0&id_t=0&page={1}&uid=0'.format(userId, page)
+		r = requests.get(url + str(userId))
+		html_parser = BeautifulSoup(r.text, 'html.parser')
+		rows = html_parser.find_all('table', {'class' : 'main refresh'})[0].findAll('tr')
+		for row in rows:
+			id, tim, user, task, lang, res, test, etime, emem = (_.text for _ in row.findAll('td'))
+			task = int(task)
+			if res == 'Accepted':
+				good_task.append(task)
+			else:
+				bad_task.append(task)
+		if len(rows) < 20:
+			break
+		page += 1
+	return (Tasks(bad_task, good_task), len(good_task) + len(bad_task))
 
 
-def writable_markdown_line(parsed):
-	result = '| '
-	for x in parsed:
-		result += str(x if type(x) != list else len(x)) + ' | '
-	return result[:-1]
+def get_time():
+	return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
-def writable_markdown_text(time_string, parsed):
+
+def writable_md(time, data):
 	result = '''# Результаты acmp
 Здесь можно увидеть результаты решения задач на сайте [acmp](https://acmp.ru). 
 
@@ -47,29 +154,22 @@ def writable_markdown_text(time_string, parsed):
 Свои результаты можно посмотреть в папке `results`.
 
 ## Таблица
-Время обновления: ''' + time_string + '\n'
-	result += '| Участник | +    | -    | Место | Рейтинг |\n'
-	result += '| -------- | ---- | ---- | ----- | ------- |\n'
-	for x in parsed:
-		result += writable_markdown_line(x) + '\n'
-	return result
-	
-
-def correct(time):
-	return '0' + str(time) if time < 10 else str(time)
-
-
-def get_time():
-	now_datetime = datetime.now()
-	now_date = now_datetime.date()
-	now_time = now_datetime.time()
-	result = str(now_date) + ' '
-	result += '{0}:{1}:{2}'.format(correct(now_time.hour), correct(now_time.minute), correct(now_time.second))
+Время обновления: {}
+| ID   | Участник | Место | Рейтинг | Посылки | + (1000) | - (1000) | +    | -    |
+| ---- | -------- | ----- | ------- | ------- | -------- | -------- | ---- | ---- |
+'''.format(time)
+	for x in data:
+		result += x[0].writable_md() + x[1].writable_md()
 	return result
 
 
-def sort_result(result):
-	return result[3]
+def writable_txt(time, data):
+	result = '''Время обновления: {}
+ID	Участник                 Место	Рейтинг	Посылки	+ (1000)   - (1000)	+	-
+'''.format(time)
+	for x in data:
+		result += x[0].writable_txt() + x[1].writable_txt()
+	return result
 
 
 def parse_all():
@@ -78,22 +178,18 @@ def parse_all():
 	result = []
 	parsing_time = get_time()
 	for now in files:
-		parsed = parse_one(now)
-		with open('results/' + parsed[1] + '.txt', 'w', encoding='utf-8') as f:
-			f.write(writable(parsed))
-		result.append([parsed[1], parsed[2], parsed[3], parsed[4], parsed[5]])
-	result = sorted(result, key=sort_result)
+		user = parse_user_profile(now)
+		tasks, user.send = parse_user_submissions(now)
+		with open('results/' + user.name + '.txt', 'w', encoding='utf-8') as f:
+			f.write(user.writable())
+			f.write(tasks.writable())
+		result.append([user, tasks])
+	result = sorted(result, key=lambda x: x[0].rank)
 	ExcelWriter().write('results.xlsx', result)
-	with open('results.txt', 'w', encoding='utf-8') as f:
-		f.write('Время обновления: ' + parsing_time + '\n')
-		f.write('Участник                 +\t-\tМесто\tРейтинг\n')
-		for now in result:
-			name = now[0]
-			while(len(name) < 25):
-				name += ' '
-			f.write(name + str(len(now[1])) + '\t' + str(len(now[2])) + '\t' + str(now[3]) + '\t' + str(now[4]) + '\n')
 	with open('README.md', 'w', encoding='utf-8') as f:
-		f.write(writable_markdown_text(parsing_time, result))
+		f.write(writable_md(parsing_time, result))
+	with open('results.txt', 'w', encoding='utf-8') as f:
+		f.write(writable_txt(parsing_time, result))
 
 
 if __name__ == '__main__':
